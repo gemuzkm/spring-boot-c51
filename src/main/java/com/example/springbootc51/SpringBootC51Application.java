00dea5a317de83dca0cd91063f41b390a26d3cc9
gemuz_work <gemuzkm@gmail.com>
2022-03-23 09:58:58 +0300

    init commit

---
+++ src/main/java/com/example/springbootc51/SpringBootC51Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootC51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootc51/SpringBootC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootC51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
