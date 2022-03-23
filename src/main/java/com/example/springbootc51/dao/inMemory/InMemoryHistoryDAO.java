8d9d471ed7a86f64f9fdc596ef18ef415ff48fe5
gemuz_work <gemuzkm@gmail.com>
2022-03-23 14:47:38 +0300

    class InMemoryHistoryDAO init

---
+++ src/main/java/com/example/springbootc51/dao/inMemory/InMemoryHistoryDAO.java
@@ -0,0 +1,28 @@
+package com.example.springbootc51.dao.inMemory;
+
+import com.example.springbootc51.dao.DAO;
+import com.example.springbootc51.entity.Operation;
+
+import java.util.List;
+
+public class InMemoryHistoryDAO implements DAO<Operation> {
+    @Override
+    public void save(Operation operation) {
+
+    }
+
+    @Override
+    public void remove(Operation operation) {
+
+    }
+
+    @Override
+    public void update(Operation operation) {
+
+    }
+
+    @Override
+    public List<Operation> findAll() {
+        return null;
+    }
+}
