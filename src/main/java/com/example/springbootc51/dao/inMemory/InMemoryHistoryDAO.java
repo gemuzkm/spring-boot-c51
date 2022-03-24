38cb5d659a74e1e88bd0dff8ac814d66f422c4ed
gemuzkm <gemuzkm@gmail.com>
2022-03-24 23:20:52 +0300

    InMemoryHistoryDAO impl

--- src/main/java/com/example/springbootc51/dao/inMemory/InMemoryHistoryDAO.java
+++ src/main/java/com/example/springbootc51/dao/inMemory/InMemoryHistoryDAO.java
@@ -1,5 +1,6 @@
 package com.example.springbootc51.dao.inMemory;
 
+import com.example.springbootc51.dao.DAO;
 import com.example.springbootc51.entity.Operation;
 import com.example.springbootc51.entity.User;
 import org.springframework.stereotype.Component;
@@ -12,21 +13,30 @@ import java.util.Map;
 
 @Component
 @Repository
-public class InMemoryHistoryDAO {
+public class InMemoryHistoryDAO implements DAO<Operation> {
 //    private static Map<Long, List<Operation>> mapOperation = new HashMap<>();
     private static List<Operation> listOperation = new ArrayList<>();
 
+    @Override
     public void save(Operation operation) {
        listOperation.add(operation);
     }
 
+    @Override
     public void remove(Operation operation) {
-
+        listOperation.removeIf(o -> (o.getUser() == operation.getUser()) && (o.getId() == operation.getId()));
     }
 
+    @Override
     public void update(Operation operation) {
 
     }
+
+    @Override
+    public List<Operation> findAll() {
+        return null;
+    }
+
 
     public List<Operation> findAll(User user) {
         List<Operation> userHistory = new ArrayList<>();
